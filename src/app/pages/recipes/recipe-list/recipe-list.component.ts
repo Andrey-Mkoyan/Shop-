d7ab5fbda127883c7fd1ba577630e5372a853a76
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2022-02-24 16:27:57 +0400

    add all important features and components

--- src/app/pages/recipes/recipe-list/recipe-list.component.ts
+++ src/app/pages/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F12%2F20%2Fsous-vide-pork-chops-FT-RECIPE1221.jpg'),
+      new Recipe(
+        'A test Recipe',
+        'This is simply a test',
+        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F12%2F20%2Fsous-vide-pork-chops-FT-RECIPE1221.jpg')
+  ];
 
   constructor() { }
 
