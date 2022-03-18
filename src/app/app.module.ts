397531b808e6bc3f389a061761f7cc3038bdae5f
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2022-03-18 15:42:09 +0400

    adding reactiv forms and new recipeEditComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,7 +37,8 @@ import { RecipeEditComponent } from './pages/recipes/recipe-edit/recipe-edit.com
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatMenuModule
+    MatMenuModule,
+    ReactiveFormsModule,
   ],
   providers: [
     RecipeService,
